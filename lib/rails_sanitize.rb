98e155a321d5d1c9919fed1f74b8e5c6a76a9270
Will Clark <will@Will.local>
2010-08-05 08:12:22 -0600

    Added logic to check for Rails 3

--- lib/rails_sanitize.rb
+++ lib/rails_sanitize.rb
@@ -2,7 +2,7 @@ require 'action_pack/version'
 
 # This class exists so including the Rails HTML sanitization helpers doesn't pollute your models.
 class RailsSanitize
-  if ActionPack::VERSION::MINOR >= 2 # Rails 2.2+
+  if ActionPack::VERSION::MAJOR == 3 || (ActionPack::VERSION::MAJOR == 2 && ActionPack::VERSION::MINOR >= 2) # Rails 2.2+
     extend ActionView::Helpers::SanitizeHelper::ClassMethods
   else # Rails 2.1 or earlier (note: xss_terminate does not support Rails 1.x)
     include ActionView::Helpers::SanitizeHelper
