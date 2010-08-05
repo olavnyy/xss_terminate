45747a1828abd70b0a4f987a8ab09c5418ba7192
Will Clark <will@Will.local>
2010-08-05 15:18:35 -0600

    Added method to handle Rails version logic

--- lib/rails_sanitize.rb
+++ lib/rails_sanitize.rb
@@ -2,9 +2,16 @@ require 'action_pack/version'
 
 # This class exists so including the Rails HTML sanitization helpers doesn't pollute your models.
 class RailsSanitize
-  if ActionPack::VERSION::MAJOR == 3 || (ActionPack::VERSION::MAJOR == 2 && ActionPack::VERSION::MINOR >= 2) # Rails 2.2+
+
+  if rails_2_or_higher
     extend ActionView::Helpers::SanitizeHelper::ClassMethods
   else # Rails 2.1 or earlier (note: xss_terminate does not support Rails 1.x)
     include ActionView::Helpers::SanitizeHelper
   end
+
+  # Determines if the current Rails version is 2.2 or higher
+  def rails_2_or_higher
+    ActionPack::VERSION::MAJOR == 3 || (ActionPack::VERSION::MAJOR == 2 && ActionPack::VERSION::MINOR >= 2)
+  end
+
 end
